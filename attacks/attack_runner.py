223785c92cb28652d88e5ab924199dc870ef730d
jmunetong <muneton.juand@gmail.com>
2023-11-14 12:44:50 -0800

    updated attack_runner

--- attacks/attack_runner.py
+++ attacks/attack_runner.py
@@ -3,30 +3,51 @@ import numpy as np
 from attack_generator import AttackGenerator
 
 from art.estimators.classification import PyTorchClassifier
+from typing import Union
+from utils import save_attacks
 
-def generate_attacks(model, data_loader, target_class, attack_list, 
-                     input_shape, num_classes, 
-                     attack_config_dict,  min_pixel_value=-1.0,
-                    max_pixel_value=1.0):
+def generate_attacks(model, data_loader, attack_list, num_samples,
+                     input_shape, num_classes, target_class: Union[None, int],
+                     attack_config_dict,  min_pixel_value=-1.0, max_pixel_value=1.0,
+                    dir_path= '../datasets'):
 
-    x_adv_dict = None
-    criterion = nn.CrossEntropyLoss()
-    clf = PyTorchClassifier(model=model, clip_values=(min_pixel_value, max_pixel_value), 
-                            loss=criterion,input_shape=input_shape, nb_classes=num_classes)
-    attack_runer = AttackGenerator(clf, target_class=target_class,attack_list= attack_list,
-                                    attack_config_dict=attack_config_dict)
     
-    for d in data_loader:
-        x , y = d[0], d[1]
-        b = y == target_class
-        idx = b.nonzero()
-        x , y = x[idx,:,:,:].squeeze(1), y[idx]
+    criterion = nn.CrossEntropyLoss()
+    clf = PyTorchClassifier(model=model,
+                            clip_values=(min_pixel_value, max_pixel_value), 
+                            loss=criterion, 
+                            input_shape=input_shape, 
+                            nb_classes=num_classes)
+    
+    for attack_name in attack_list:
+        attack_name = [attack_name] if isinstance(attack_name, str) else attack_name
+        attack_runer = AttackGenerator(clf, target_class=target_class,
+                                       attack_list= [attack_name],
+                                        attack_config_dict=attack_config_dict)
+        x_adv = []
+        y_adv = []
+        count = 0
+        
+        for d in data_loader:
+            x , y = d[0], d[1]
+            if target_class is not None:
+                b = y == target_class
+                idx = b.nonzero()
+                x , y = x[idx,:,:,:].squeeze(1), y[idx]
 
-        x_adv_i = attack_runer.generate_attack(x.numpy(), y=None)
-        if x_adv_dict is None:
-            x_adv_dict = x_adv_i
-        else:
-            for key in x_adv_dict.keys() | x_adv_i.keys():
-                x_adv_dict[key] = np.concatenate([x_adv_dict.get(key, np.array([])), 
-                                                  x_adv_i.get(key, np.array([]))])
-    return x_adv_dict
+            x_adv_i = attack_runer.generate_attack(x.numpy(), y=None)[attack_name]
+            y_adv.append(y)
+            x_adv.append(x_adv_i)
+            count += len(x)
+            if count > num_samples:
+                break
+        
+        sample_shape = x_adv_i.shape[1:]
+        x_adv = np.array(x_adv).reshape(-1, *sample_shape)
+        y_adv = np.array(y_adv).reshape(-1)
+        save_attacks(dir_path, attack_name, x, y)
+
+
+
+
+        
--- attacks/utils.py
+++ attacks/utils.py
@@ -1,6 +1,7 @@
 import yaml
 import h5py
 
+import os
 
 def load_yaml_file(file_path):
     with open(file_path, 'r') as file:
@@ -11,19 +12,11 @@ def load_yaml_file(file_path):
             print(f"Error loading YAML file {file_path}: {e}")
             return None
         
-def save_attacks(self, attack_dataset_dict: dict):
-    if '.hdf5' not in self.file_name:
-        self.file_name += '.hdf5'
-    with h5py.File(self.file_name, 'w') as f:
-                # Create a dataset and write data to it
-        for attack_name in attack_dataset_dict:
-            if attack_name in f:
-                # Dataset exists, append data
-                existing_dataset = f[attack_name]
-                print(existing_dataset.shape)
-                existing_dataset.resize((existing_dataset.shape[0] + attack_dataset_dict[attack_name].shape[0],) + existing_dataset.shape[1:])
-                existing_dataset[-attack_dataset_dict[attack_name].shape[0]:] = attack_dataset_dict[attack_name]
-            else:
-                # Dataset does not exist, create a new dataset
-                f.create_dataset(attack_name, data=attack_dataset_dict[attack_name])
+def save_attacks(directory_path, attack_name, x, y):
+    file_name = os.path.join(directory_path, attack_name)
+    if '.hdf5' not in file_name:
+        file_name += '.hdf5'
+    with h5py.File(file_name, 'w') as f:
+        f.create_dataset('x', data=x)
+        f.create_dataset('y', data=y)
 
