83a821df4e13b5da534180bafab981365f41f6b2
jmunetong <muneton.juand@gmail.com>
2023-11-15 09:51:23 -0800

    added jsd and kl divergence metrics for attack pipeline

--- attacks/metrics.py
+++ attacks/metrics.py
@@ -1,12 +1,10 @@
-from scipy.stats import wasserstein_distance 
+from scipy.stats import wasserstein_distance, entropy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-
-
-def compute_wasserstein(metrics, y, y_pred, y_adv, u_weight=None, v_weight=None, 
+def compute_wasserstein(y, y_pred, y_adv, u_weight=None, v_weight=None, 
                         clf_log_softmax:bool = False, log_target: bool=False):
     if isinstance(y, torch.Tensor):
         y = y.numpy()
@@ -20,43 +18,82 @@ def compute_wasserstein(metrics, y, y_pred, y_adv, u_weight=None, v_weight=None,
 
     if log_target:
         y = np.exp(y)
-    metrics['wasserstein']['x'] = wasserstein_distance(y, y_pred)
-    metrics['wasserstein']['x_adv'] = wasserstein_distance(y, y_adv)
+    
+    x = wasserstein_distance(y, y_pred, u_weights=u_weight, v_weights=v_weight)
+    x_adv = wasserstein_distance(y, y_adv, u_weights=u_weight, v_weights=v_weight)
+
+    return x, x_adv
+
+def compute_jensen_shannon_distance(y, y_pred, y_adv, 
+                                    clf_log_softmax:bool = False, 
+                                    log_target: bool=False):
+    if isinstance(y, torch.Tensor):
+        y = y.numpy()
+    if isinstance(y_adv, torch.Tensor):
+        y_adv = y_adv.numpy()
+    if isinstance(y_pred, torch.Tensor):
+        y_pred = y_pred.numpy()
+
+    if clf_log_softmax:
+        y_pred, y_adv = np.exp(y_pred), np.exp(y_adv)
+
+    if log_target:
+        y = np.exp(y)
+
+    m = (y + y_pred) / 2
+    x = np.sqrt((entropy(y, m) + entropy(y_pred, m)) / 2)
+    m = (y + y_adv) / 2
+    x_adv = np.sqrt((entropy(y, m) + entropy(y_adv, m)) / 2)
+    return x, x_adv
 
 
-def compute_kl_div(metrics: dict, y, y_pred, y_pred_adv, clf_log_softmax: bool, log_target: bool):
+def compute_kl_div(y, y_pred, y_pred_adv, clf_log_softmax: bool, 
+                   log_target: bool):
     if not clf_log_softmax:
         y_pred = F.log_softmax(y_pred, dim=1)
         y_pred_adv =  F.log_softmax(y_pred_adv, dim=1)
     kl_loss = nn.KLDivLoss(reduction='mean', log_target=log_target)
     kl_x = kl_loss(y_pred, y)
     kl_x_adv = kl_loss(y_pred_adv, y)
-    metrics['kl']['x'] = kl_x.item()
-    metrics['kl']['x_adv'] = kl_x_adv.item()
+    x = kl_x.item()
+    x_adv = kl_x_adv.item()
+    return x, x_adv
 
     
-def compute_distributional_distances(data_loader, clf, save_file, log_target: bool=False, 
+def compute_distributional_distances(data_loader, clf, log_target: bool=False, 
                                        clf_log_softmax: bool = False):
-    metrics = {'kl',
-               'wasserstein'
-               }
-    #Ensure this dataloader is not shuffled
+    metrics = {}
     clf.eval()
-    _, _, y = data_loader.dataset[:]
     data_loader = iter(data_loader)
     y_pred = []
     y_pred_adv =  []
-    for x_i, x_adv_i, _ in data_loader:
+    y = []
+    for x_i, x_adv_i, y_i in data_loader:
         y_pred_i = clf(x_i)
         y_pred_adv_i = clf(x_adv_i)
         y_pred.append(y_pred_i)
         y_pred_adv.append(y_pred_adv_i)
-    
+        y.append(y_i)
 
     y_pred = torch.cat(y_pred, dim=0)
     y_pred_adv = torch.cat(y_pred_adv, dim=0)
-    compute_kl_div(metrics, y, y_pred, y_pred_adv, 
+    y = torch.cat(y, dim=0)
+    
+    x, x_adv = compute_kl_div(y, y_pred, y_pred_adv, 
                    clf_log_softmax=clf_log_softmax, log_target=log_target)
+    metrics['kl'] = {'x': x, 'x_adv': x_adv}
+    
+    x, x_adv= compute_wasserstein(y, y_pred, y_pred_adv, clf_log_softmax=clf_log_softmax, 
+                                  log_target=log_target)
+    metrics['wasserstein'] = {'x': x, 'x_adv': x_adv}
+    x, x_adv = compute_jensen_shannon_distance(y, y_pred, y_pred_adv,  
+                        clf_log_softmax, log_target)
+    metrics['jsd'] = {'x': x, 'x_adv': x_adv}
+
+    return metrics
+
+
+
 
 
 
